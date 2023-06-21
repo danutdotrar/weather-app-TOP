4fff058d47fed48504cf34ceddd9d118d670a665
danutdotrar <morarasu.danut@gmail.com>
2023-06-21 17:10:45 +0300

    Add HTML structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import "./style.css";
 
 // Working with APIs (Application Programming Interfaces)
-fetch(
-    "https://api.weatherapi.com/v1/current.json?key=70f95910843b4d8893b143641231806&q=london"
-)
-    .then((res) => res.json())
-    .then(getTemp);
+// // Promises
+// fetch(
+//     "https://api.weatherapi.com/v1/current.json?key=70f95910843b4d8893b143641231806&q=london"
+// )
+//     .then((res) => res.json())
+//     .then(getTemp);
 
-function getTemp(data) {
+// function getTemp(data) {
+// console.log(data);
+// }
+
+// Get the data from the api
+async function getDataFromLocation(location) {
+    const res = await fetch(
+        `https://api.weatherapi.com/v1/current.json?key=70f95910843b4d8893b143641231806&q=${location}`
+    );
+    const data = await res.json();
+
     console.log(data);
 }
+
+getDataFromLocation("new york");
